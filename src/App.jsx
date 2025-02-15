01b2cebee5a5b01e3d9f74e1731b865146d07f8b
kushal gowda P <kushalgowda8586@gmail.com>
2025-02-15 09:29:56 +0530

    added homepage

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import react from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='homepage'>
+      <h2>Kushal gowda .P</h2>
+      <ul>
+        <li>kushalgowda8586@gmail.com</li>
+        <li>+91 9108783534</li>
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default App
